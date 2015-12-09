5c4f981a0aad1b0cd7d4bcb7f2d2e3212515891d
Soichi Tanabe <soichit@uw.edu>
2015-12-09 03:20:31 -0800

    made layout look cool, small touches

--- js/zoom.js
+++ js/zoom.js
@@ -4,29 +4,28 @@ var baseUrl = 'http://babel-us-east-1.eigenfactor.org/search?q=';
 var myApp = angular.module('myApp', []);
 
 myApp.controller('myCtrl', function($scope, $http) {
-    //$scope.selected = ""
+    $scope.selected = ""
 
     $scope.getSearch = function() {
         $http.get(baseUrl + $scope.usrSearch).success(function(response){
-            $http.get(baseUrl + $scope.usrSearch + "&start=10").success(function(responseNext){
-                $http.get(baseUrl + $scope.usrSearch + "&start=20").success(function(responseLast){  
-                    console.log("response.results: " + response.results)
-                    console.log("responseNext.results: " + responseNext.results)
-                    
-                    var sum = response.results.concat(responseNext.results);
-                    //console.log("sum1: " + sum.length);
-                    sum = sum.concat(responseLast.results);
-                    //console.log("sum2: " + sum.length);
-                    console.log(sum);
+            $http.get(baseUrl + $scope.usrSearch + "&start=10").success(function(response2){
+                $http.get(baseUrl + $scope.usrSearch + "&start=20").success(function(response3){ 
+                    $http.get(baseUrl + $scope.usrSearch + "&start=30").success(function(response4){
+                        var sum = response.results;
+                        sum = sum.concat(response2.results);
+                        sum = sum.concat(response3.results);
+                        sum = sum.concat(response4.results);
+                        console.log(sum);
 
-                    //case where no results are found
-                    if (sum == "") {
-                        $scope.selected = "No results found"
-                    } else {
-                        $scope.selected = $scope.usrSearch
-                        root = $scope.bubbles = buildHierarchy(sum) 
-                        console.log(root)
-                    }
+                        //case where no results are found
+                        if (sum == "") {
+                            $scope.selected = "No results found"
+                        } else {
+                            $scope.selected = $scope.usrSearch
+                            root = $scope.bubbles = buildHierarchy(sum) 
+                            console.log(root)
+                        }
+                    })
                 })
             })
         })
@@ -98,31 +97,24 @@ var myDir = myApp.directive("bubbleChart", function($window) {
         //Wrapper element to put your svg chart in
         wrapper = d3.select(elem[0]);
 
-       // console.log(scope.root + "");
-
         scope.$watch('root', function(){
             if (scope.root == undefined) return
-            //console.log(scope.root)
             draw()
         })
 
         var margin = 20,
             diameter = 960;
+        diameter.id = ("diameter");
 
         var color = d3.scale.linear()
             .domain([-1, 5])
             .range(["hsl(152,80%,80%)", "hsl(228,30%,40%)"])
-
-            //experiment with rgb colors
-            //.range(["hsl(52,80%,80%)", "hsl(128,30%,40%)"])
             .interpolate(d3.interpolateHcl);
-
-        //color.range(["hsl(52,80%,80%)", "hsl(128,30%,40%)"]);
 
         var pack = d3.layout.pack()
             .padding(2)
             .size([diameter - margin, diameter - margin])
-            .value(function(d) { return d.score })//d.depth;})//(d.score * 1000)})
+            .value(function(d) { return d.score })
 
         var svg = wrapper.append("svg")
             .attr("width", diameter)
@@ -130,14 +122,6 @@ var myDir = myApp.directive("bubbleChart", function($window) {
             .append("g")
                 .attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
 
-        // Circle positioning function
-        /*var circleFunc = function(circle) {
-            circle.attr('cx', function(d){return d.x})
-                .attr('cy', function(d){return d.y})
-                .attr('r', function(d){return d.r})
-                .attr('fill', function(d) {
-                })
-        }*/
 
         var draw = function() {     
             // Make a copy of your data, stored in an object {children:FILTERED-DATA}
@@ -151,22 +135,8 @@ var myDir = myApp.directive("bubbleChart", function($window) {
 
             var circle = svg.selectAll("circle")
                 .data(nodes, function(d) {return d.title});
-                //.data(nodes)
             circle.enter().append("circle")
                 .attr("class", function(d) { return d.parent ? d.children ? "node" : "node node--leaf" : "node node--scope.root"; })
-                
-                //attempt to change circle colors with if statements
-                /*
-                var booleanColor = function(d) {
-                    return d.title.length < 8
-                }
-                if (booleanColor) {
-                    color.range(["hsl(52,80%,80%)", "hsl(128,30%,40%)"])
-                } else {
-                    color.range(["hsl(152,80%,80%)", "hsl(228,30%,40%)"])
-                };
-                */
-
                 .style("fill", function(d) { return d.children ? color(d.depth) : null; })
                 //.attr('r', function(d){return 1000})
                 .on("click", function(d) { if (focus !== d) zoom(d), d3.event.stopPropagation(); })
@@ -180,32 +150,19 @@ var myDir = myApp.directive("bubbleChart", function($window) {
                     d3.select(this).select("circle").style("display", "none");
                 });
 
-            //attempt to change each circle color and size
-            //color.range(["hsl(52,80%,80%)", "hsl(128,30%,40%)"])
-            svg.selectAll("circle")
-                .attr("r", 100)
-                //.style("fill", "red");
-
-
             //solution to solve second search glitch
             svg.selectAll("text").remove();
 
-            console.log("scope.filteredData" + scope.filteredData);
             var text = svg.selectAll("text")
                     .data(nodes);
-            console.log(nodes)
-            console.log("nodes.children: " + nodes.children);
 
                 text.enter().append("text")
                     .attr("class", "label")
                     .attr("id", "textwrap")
                     .style("opacity", function(d) { return d.parent === scope.filteredData ? 0.5 : 1; })
                     .style("display", function(d) { return d.parent === scope.filteredData ? "block" : "none"; })
-                    //.style("font-size", function(d) { return d.parent === scope.filteredData ? 25 : 15; })
-                    .style("font-size", function(d) { return (d.children !== undefined && d.children.length > 1) ? 25 : 15; })          
+                    .style("font-size", function(d) { return (d.children !== undefined && d.children.length == 1) ? 10 : 30; })      
                     .text(function(d) { return d.title});
-            //d3plus.textwrap()
-                //.container(d3.select("#textwrap"));
             
 
             var node = svg.selectAll("circle,text");
